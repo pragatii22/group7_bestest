ede066c278eaf8b4eebb5e59fdc29b71ec48a5a0
shrinakaphle <shrinakaphle@gmail.com>
2025-11-09 14:49:47 +0545

    new file added

---
+++ week 1/shrina/skip.java
@@ -0,0 +1,23 @@
+public class skip{
+    public static void main(String[]args){
+        for(int i=1;i<=30;i++){
+            if(i==15){
+                System.out.println("Skip");}
+                else
+
+                if (i%4==0){
+                    System.out.println(i*2); 
+                }else{
+                        System.out.println(i);
+
+                }
+
+                {
+                    System.out.println("done");
+                }
+            }
+         }
+    }
+    
+        
+    
